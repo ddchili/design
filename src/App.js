6b4794def4a9559148b5cb17c61f6c451e264ca7
David Demmer <ddemmer@planfu.com>
2019-10-04 13:06:51 -0600

    test

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>helllo world</p>
+        <p>helllows world</p>
         <a
           className="App-link"
           href="https://reactjs.org"
