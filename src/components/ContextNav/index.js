43044b2337144491d9bd5448adad70b0f09137a5
ddemmer <ddemmer@lifetimefitness.com>
2020-05-30 19:13:53 -0600

    mobile styles

--- src/components/ContextNav/index.js
+++ src/components/ContextNav/index.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useEffect, useRef, useState } from 'react'
+import { isMobile } from 'react-device-detect'
 import AppBar from '@material-ui/core/AppBar'
 import './styles.scss'
 
@@ -9,9 +10,10 @@ export default function ContextNav(props){
 
   const [isSticky, setSticky] = useState(false)
   const ref = useRef(null)
+  const top = isMobile ? 0 : 94
   const handleScroll = () => {
     if (ref.current) {
-      setSticky(ref.current.getBoundingClientRect().top <= 94)
+      setSticky(ref.current.getBoundingClientRect().top <= top)
     }
   };
 
